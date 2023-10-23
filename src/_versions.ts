0a3fb0a26ced7661ed810e47dceebe91b42b0f37
reda <tarzaltreda@gmail.com>
2023-10-23 09:47:20 -0400

    issue #23 : update workflow

--- src/_versions.ts
+++ src/_versions.ts
@@ -11,8 +11,8 @@ export interface TsAppVersion {
 export const versions: TsAppVersion = {
     version: '0.1.0',
     name: 'louis-finesse',
-    versionDate: '2023-07-27T19:01:19.184Z',
-    gitCommitHash: '4eed505',
-    versionLong: '0.1.0-4eed505',
+    versionDate: '2023-10-23T13:08:26.119Z',
+    gitCommitHash: '9d3f0ff',
+    versionLong: '0.1.0-9d3f0ff',
 };
 export default versions;
