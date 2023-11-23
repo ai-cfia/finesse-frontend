c45c09e309ba4135df4bb5f39a5dfd535a521d03
reda <tarzaltreda@gmail.com>
2023-11-23 18:05:52 +0000

    parent 4c130e324537ae45ec7fd5698c74e24dbe2b72e9 author reda <tarzaltreda@gmail.com> 1700762752 +0000 committer reda <tarzaltreda@gmail.com> 1703009832 -0500
    issue #38: add deployment file
    
    issue #35: testing deployment with changes
    
    issue #35: minor fix
    
    issue #16: fix placeholder query + tests
    
    issue #16: update version

--- src/_versions.ts
+++ src/_versions.ts
@@ -9,10 +9,10 @@ export interface TsAppVersion {
     gitTag?: string;
 };
 export const versions: TsAppVersion = {
-    version: '0.2.0',
+    version: '0.2.2',
     name: 'finesse-frontend',
-    versionDate: '2023-12-04T21:25:19.734Z',
-    gitCommitHash: '1b203ac',
-    versionLong: '0.2.0-1b203ac',
+    versionDate: '2023-12-14T10:10:26.754Z',
+    gitCommitHash: '93e9f5d',
+    versionLong: '0.2.2-93e9f5d',
 };
 export default versions;
