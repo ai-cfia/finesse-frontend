30df918fe8d79ee60a017ceedede1aa6ed28a5c6
K. Allagbe <kotchikpa.allagbe@gmail.com>
2023-12-04 13:25:53 -0500

    issue #46: update version to v0.2.0

--- src/_versions.ts
+++ src/_versions.ts
@@ -9,10 +9,10 @@ export interface TsAppVersion {
     gitTag?: string;
 };
 export const versions: TsAppVersion = {
-    version: '0.1.0',
+    version: '0.2.0',
     name: 'finesse-frontend',
-    versionDate: '2023-12-04T15:23:08.767Z',
-    gitCommitHash: 'a66ef17',
-    versionLong: '0.1.0-a66ef17',
+    versionDate: '2023-12-04T18:23:32.498Z',
+    gitCommitHash: '872070a',
+    versionLong: '0.2.0-872070a',
 };
 export default versions;
