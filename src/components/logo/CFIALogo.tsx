d0b291cc553b3b2b95dc4e29c7dbc29e3a59f95f
Youllou <juless.barbier@gmail.com>
2023-12-06 17:13:04 -0500

    added a baseUrl variable to CFIALogo.tsx and modified the href prop so it redirect to /+baseUrl

--- src/components/logo/CFIALogo.tsx
+++ src/components/logo/CFIALogo.tsx
@@ -2,12 +2,14 @@ import React from "react"; // Import 'React' from the 'react' package.
 import cfia from "../../assets/CFIA_SmartSearch_Logo.png";
 import styles from "../../pages/home/Home.module.css";
 
+const baseUrl: string = process.env.REACT_APP_BASENAME as string;
+
 // Header Component -> Displays CFIA banner image.
 const CFIALogo: React.FC = () => {
   // Add type annotation for the component.
   return (
     <div>
-      <a href="/" title="ACIA | CFIA">
+      <a href={"/" + baseUrl} title="ACIA | CFIA">
         <img src={cfia} alt="CFIA logo" className={styles.githubLogo} />
       </a>
     </div>
