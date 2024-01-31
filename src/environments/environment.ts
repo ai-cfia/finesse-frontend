f52040f083e8b59af78ff57adf394777a7e569f9
K. Allagbe <kotchikpa.allagbe@gmail.com>
2024-01-31 14:40:24 -0500

    issue #77: reset project

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-import versions from "../_versions";
-export const environment = {
-  production: false,
-  version: versions.version,
-};
--- src/react-app-env.d.ts
+++
@@ -1,1 +0,0 @@
-import "react-scripts"; // or import 'react-scripts/scripts/react-app' if you don't want to eject
